225c818d360db5de27ad9eb556727b112476663b
xchee-01 <xchee-01@nus.edu.sg>
2025-05-11 15:46:38 +0800

    Update template_JS_learning_page_v17.js

--- PMTHA/template_JS_learning_page_v17.js
+++ PMTHA/template_JS_learning_page_v17.js
@@ -5,8 +5,8 @@ const COPY_TRACKING_URL = 'https://script.google.com/macros/s/AKfycbz88biGq3GAIf
 const EXTRA_INFO_INTERACTION_URL = 'https://script.google.com/macros/s/AKfycbxXHMDDEnZq0krPyE96d22zEp3DqdLmwO74OsxSRW1Rq_JvHEYRXc5IgnaV-6J1l4o_GQ/exec';
 
 // Configuration and constants
-const TRACKING_INTERVAL = 2000; // 2 seconds for periodic tracking
-const HEARTBEAT_INTERVAL = 2000; // 2 seconds for heartbeat
+const TRACKING_INTERVAL = 5000; // 5 seconds for periodic tracking
+const HEARTBEAT_INTERVAL = 5000; // 5 seconds for heartbeat
 const INACTIVITY_THRESHOLD = 10000; // 10 seconds until user is considered inactive
 const THROTTLE_DELAY = 1000; // Throttle delay for frequent events
 
