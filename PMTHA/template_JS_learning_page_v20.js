5126c3c051caec5cc523dcf7090a33bf995b238b
xchee-01 <xchee-01@nus.edu.sg>
2025-05-13 17:06:05 +0800

    Update template_JS_learning_page_v20.js

--- PMTHA/template_JS_learning_page_v20.js
+++ PMTHA/template_JS_learning_page_v20.js
@@ -7,9 +7,9 @@ const COPY_TRACKING_URL = 'https://script.google.com/macros/s/AKfycbz88biGq3GAIf
 const EXTRA_INFO_INTERACTION_URL = 'https://script.google.com/macros/s/AKfycbxXHMDDEnZq0krPyE96d22zEp3DqdLmwO74OsxSRW1Rq_JvHEYRXc5IgnaV-6J1l4o_GQ/exec';
 
 // Configuration and constants
-const TRACKING_INTERVAL = 60000; // 60 seconds for periodic tracking 
-const HEARTBEAT_INTERVAL = 5000; // 5 seconds for heartbeat 
-const INACTIVITY_THRESHOLD = 20000; // 10 seconds until user is considered inactive
+const TRACKING_INTERVAL = 20000; // 20 seconds for periodic tracking 
+const HEARTBEAT_INTERVAL = 1000; // 1 seconds for heartbeat 
+const INACTIVITY_THRESHOLD = 10000; // 10 seconds until user is considered inactive
 const THROTTLE_DELAY = 1000; // Throttle delay for frequent events
 const LOCAL_STORAGE_KEY = 'tracking_events_cache'; // Key for localStorage tracking cache
 const EVENT_BATCH_THRESHOLD = 100; // Minimum number of events before sending to server
